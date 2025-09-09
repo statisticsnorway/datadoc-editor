cc101944796ab1270fb6d8698a061663c422a364
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-09-09 13:43:07 +0200

    remove unused pseudo button

--- src/datadoc_editor/frontend/components/builders.py
+++ src/datadoc_editor/frontend/components/builders.py
@@ -213,31 +213,6 @@ def build_variables_pseudonymization_section(
         className="variable-section",
     )
 
-
-def build_variables_pseudo_button(title: str, short_name: str) -> html.Section:
-    """Create the button to add pseudonymization."""
-    return html.Section(
-        children=[
-            ssb.Title(title, size=3, className="edit-section-title"),
-            ssb.Button(
-                children=["Legg til pseudonymisering"],
-                id={
-                    "type": "pseudo-button",
-                    "short_name": short_name,
-                },
-                className="add-pseudo-button",
-            ),
-            html.Div(
-                id={
-                    "type": "pseudo-output",
-                    "short_name": short_name,
-                }
-            ),
-        ],
-        className="variable-pseudo-button",
-    )
-
-
 def build_ssb_accordion(
     header: str,
     key: dict,
