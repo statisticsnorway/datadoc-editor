737b7602ef227b38221e0d423963a92145ff110b
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-09-10 08:41:05 +0200

    create test class for pseudo and move existing pseudo test

--- tests/frontend/components/test_build_input_section.py
+++ tests/frontend/components/test_build_input_section.py
@@ -6,25 +6,10 @@ import ssb_dash_components as ssb  # type: ignore[import-untyped]
 from dapla_metadata.datasets import model
 
 from datadoc_editor.frontend.components.builders import build_input_field_section
-from datadoc_editor.frontend.components.builders import (
-    build_variables_pseudonymization_section,
-)
 from datadoc_editor.frontend.fields.display_base import MetadataCheckboxField
 from datadoc_editor.frontend.fields.display_base import MetadataDropdownField
 from datadoc_editor.frontend.fields.display_base import MetadataInputField
 from datadoc_editor.frontend.fields.display_base import MetadataPeriodField
-from datadoc_editor.frontend.fields.display_pseudo_variables import (
-    PSEUDONYMIZATION_DEAD_METADATA,
-)
-from datadoc_editor.frontend.fields.display_pseudo_variables import (
-    PSEUDONYMIZATION_METADATA,
-)
-from datadoc_editor.frontend.fields.display_pseudo_variables import (
-    PSEUDONYMIZATION_PAPIS_WITH_STABILE_ID_METADATA,
-)
-from datadoc_editor.frontend.fields.display_pseudo_variables import (
-    PSEUDONYMIZATION_PAPIS_WITHOUT_STABILE_ID_METADATA,
-)
 from datadoc_editor.frontend.fields.display_variables import VARIABLES_METADATA_LEFT
 from datadoc_editor.frontend.fields.display_variables import VARIABLES_METADATA_RIGHT
 
@@ -231,55 +216,3 @@ def test_build_input_fields_dropdown_components(
         strict=False,
     ):
         assert item1.items == item2.options_getter()
-
-
-PSEDUO_VARIABLES_METADATA = PSEUDONYMIZATION_METADATA
-
-SELECTED_ALGORITHMS = [
-    PSEUDONYMIZATION_PAPIS_WITH_STABILE_ID_METADATA,
-    PSEUDONYMIZATION_PAPIS_WITHOUT_STABILE_ID_METADATA,
-    PSEUDONYMIZATION_DEAD_METADATA,
-]
-
-PSEUDO_INPUT_FIELD_SECTION = [
-    (
-        SELECTED_ALGORITHMS,
-        model.Variable(short_name="hoveddiagnose"),
-    ),
-]
-
-
-@pytest.mark.usefixtures("_code_list_fake_classifications")
-@pytest.mark.parametrize(
-    ("selected_algorithm", "variable"),
-    PSEUDO_INPUT_FIELD_SECTION,
-)
-def test_build_pseudo_input_section(
-    selected_algorithm,
-    variable,
-):
-    variable.pseudonymization = model.Pseudonymization()
-    input_section = build_variables_pseudonymization_section(
-        "Pseudonymisert", variable, selected_algorithm
-    )
-
-    elements_of_input = [
-        element for element in input_section.children if isinstance(element, ssb.Input)
-    ]
-
-    variable_identifier_input = [
-        element
-        for element in PSEUDONYMIZATION_METADATA
-        if isinstance(element, MetadataInputField)
-    ]
-
-    assert all(isinstance(field, ssb.Input) for field in elements_of_input)
-    assert all(item.debounce is True for item in elements_of_input)
-    assert all(
-        item1.label == item2.display_name
-        for item1, item2 in zip(
-            elements_of_input,
-            variable_identifier_input,
-            strict=False,
-        )
-    )
---
+++ tests/frontend/components/test_build_pseudo_section.py
@@ -0,0 +1,61 @@
+import pytest
+from datadoc_editor.frontend.components.builders import build_variables_pseudonymization_section
+from datadoc_editor.frontend.fields.display_base import MetadataInputField
+from datadoc_editor.frontend.fields.display_pseudo_variables import PSEUDONYMIZATION_DEAD_METADATA, PSEUDONYMIZATION_METADATA, PSEUDONYMIZATION_PAPIS_WITH_STABILE_ID_METADATA, PSEUDONYMIZATION_PAPIS_WITHOUT_STABILE_ID_METADATA
+import ssb_dash_components as ssb  # type: ignore[import-untyped]
+from dapla_metadata.datasets import model
+
+
+PSEDUO_VARIABLES_METADATA = PSEUDONYMIZATION_METADATA
+
+SELECTED_ALGORITHMS = [
+    PSEUDONYMIZATION_PAPIS_WITH_STABILE_ID_METADATA,
+    PSEUDONYMIZATION_PAPIS_WITHOUT_STABILE_ID_METADATA,
+    PSEUDONYMIZATION_DEAD_METADATA,
+]
+
+PSEUDO_INPUT_FIELD_SECTION = [
+    (
+        SELECTED_ALGORITHMS,
+        model.Variable(short_name="hoveddiagnose"),
+    ),
+]
+
+
+@pytest.mark.usefixtures("_code_list_fake_classifications")
+@pytest.mark.parametrize(
+    ("selected_algorithm", "variable"),
+    PSEUDO_INPUT_FIELD_SECTION,
+)
+def test_build_pseudo_input_section(
+    selected_algorithm,
+    variable,
+):
+    variable.pseudonymization = model.Pseudonymization()
+    input_section = build_variables_pseudonymization_section(
+        "Pseudonymisert", variable, selected_algorithm
+    )
+
+    elements_of_input = [
+        element for element in input_section.children if isinstance(element, ssb.Input)
+    ]
+
+    variable_identifier_input = [
+        element
+        for element in PSEUDONYMIZATION_METADATA
+        if isinstance(element, MetadataInputField)
+    ]
+
+    assert all(isinstance(field, ssb.Input) for field in elements_of_input)
+    assert all(item.debounce is True for item in elements_of_input)
+    assert all(
+        item1.label == item2.display_name
+        for item1, item2 in zip(
+            elements_of_input,
+            variable_identifier_input,
+            strict=False,
+        )
+    )
+    
+    
+# build_pseudo_field_section
