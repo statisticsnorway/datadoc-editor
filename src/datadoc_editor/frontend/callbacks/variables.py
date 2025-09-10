540342dffccac94e4766e8c591c802ef9b31c2eb
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-09-10 21:06:46 +0200

    add type

--- src/datadoc_editor/frontend/callbacks/variables.py
+++ src/datadoc_editor/frontend/callbacks/variables.py
@@ -7,6 +7,7 @@ import urllib.parse
 from typing import TYPE_CHECKING
 
 from datadoc_editor import state
+from datadoc_editor.enums import PseudonymizationAlgorithmsEnum
 from datadoc_editor.frontend.callbacks.utils import MetadataInputTypes
 from datadoc_editor.frontend.callbacks.utils import find_existing_language_string
 from datadoc_editor.frontend.callbacks.utils import map_dropdown_to_pseudo
@@ -374,7 +375,9 @@ def set_variables_values_inherit_dataset_derived_date_values() -> None:
             )
 
 
-def populate_pseudo_workspace(variable: model.Variable, selected_algorithm) -> dbc.Form:
+def populate_pseudo_workspace(
+    variable: model.Variable, selected_algorithm: PseudonymizationAlgorithmsEnum | None
+) -> dbc.Form:
     if variable is None:
         logger.info("Variable not found in lookup!")
         return []
