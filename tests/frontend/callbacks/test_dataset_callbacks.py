5b3825e8be6712287cc2e55b34f3bcb0ac0233aa
Jorgen-5 <jrussnes@gmail.com>
2025-09-22 13:50:13 +0200

    Handle deletion of use-restriction-items (#525)
    * Fixed ids
    
    * Now able to remove items
    
    * precommit
    
    * precommit
    
    * precommit
    
    * Fixed tests
    
    * Fixed utils
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests
    
    * update file
    
    * Styling
    
    * Make date field smaller
    
    * Add title
    
    * precommit
    
    * Update package
    
    * revert
    
    * Updated lock file
    
    * Fix dapla toolbelt metadata version
    
    * Fix dapla toolbelt metadata to 0.9.1
    
    * Make constants
    
    * Remove text
    
    * Fix bug
    
    * pre commit
    
    * Simplify code
    
    * Fix mypy
    
    * Fix error causing dates to disappear
    
    * Add tests
    
    * Update src/datadoc_editor/frontend/fields/display_base.py
    
    Co-authored-by: Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jorgen-5 <rlj@ssb.no>
    Co-authored-by: Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>

--- tests/frontend/callbacks/test_dataset_callbacks.py
+++ tests/frontend/callbacks/test_dataset_callbacks.py
@@ -23,6 +23,7 @@ from datadoc_editor.frontend.callbacks.dataset import accept_dataset_metadata_in
 from datadoc_editor.frontend.callbacks.dataset import accept_dataset_multidropdown_input
 from datadoc_editor.frontend.callbacks.dataset import open_dataset_handling
 from datadoc_editor.frontend.callbacks.dataset import process_special_cases
+from datadoc_editor.frontend.callbacks.dataset import remove_dataset_multidropdown_input
 from datadoc_editor.frontend.callbacks.utils import MultidropdownInputTypes
 from datadoc_editor.frontend.callbacks.utils import dataset_control
 from datadoc_editor.frontend.constants import INVALID_DATE_ORDER
@@ -226,6 +227,123 @@ def test_accept_dataset_multidropdown_input_valid_data(
         getattr(state.metadata.dataset, metadata_identifier.value)
         == expected_model_value
     )
+
+
+@pytest.mark.parametrize(
+    ("initial_list", "index_to_remove", "expected_list"),
+    [
+        (
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=None,
+                )
+            ],
+            10,
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=None,
+                )
+            ],
+        ),
+        (
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=datetime.date(2025, 9, 1),
+                ),
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=datetime.date(2023, 9, 1),
+                ),
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=datetime.date(2022, 9, 1),
+                ),
+            ],
+            1,
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=datetime.date(2025, 9, 1),
+                ),
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=datetime.date(2022, 9, 1),
+                ),
+            ],
+        ),
+        ([], 0, []),
+        (None, 0, None),
+        (
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=None,
+                )
+            ],
+            -1,
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=None,
+                )
+            ],
+        ),
+        (
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=None,
+                ),
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=None,
+                ),
+            ],
+            0,
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=None,
+                ),
+            ],
+        ),
+        (
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=None,
+                ),
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.PROCESS_LIMITATIONS.value,
+                    use_restriction_date=None,
+                ),
+            ],
+            1,
+            [
+                model.UseRestrictionItem(
+                    use_restriction_type=enums.UseRestrictionType.DELETION_ANONYMIZATION.value,
+                    use_restriction_date=None,
+                ),
+            ],
+        ),
+    ],
+)
+def test_remove_dataset_multidropdown_input_parametrized(
+    metadata: Datadoc,
+    initial_list: list[model.UseRestrictionItem],
+    index_to_remove: int,
+    expected_list: list[model.UseRestrictionItem],
+):
+    state.metadata = metadata
+    state.metadata.dataset.use_restrictions = initial_list
+    remove_dataset_multidropdown_input(
+        metadata_identifier="use_restrictions", index=index_to_remove
+    )
+    actual_list = state.metadata.dataset.use_restrictions
+    assert actual_list == expected_list
 
 
 def test_accept_dataset_metadata_input_incorrect_data_type(metadata: Datadoc):
