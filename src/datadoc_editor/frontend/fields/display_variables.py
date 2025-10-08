4a088a727266fa101c5cc0c579564a19a3383e6a
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-08 15:03:02 +0200

    add deselect id

--- src/datadoc_editor/frontend/fields/display_variables.py
+++ src/datadoc_editor/frontend/fields/display_variables.py
@@ -8,6 +8,7 @@ from enum import Enum
 from dapla_metadata.datasets import enums
 
 from datadoc_editor import state
+from datadoc_editor.constants import DELETE_SELECTED
 from datadoc_editor.enums import DataType
 from datadoc_editor.enums import TemporalityTypeType
 from datadoc_editor.enums import VariableRole
@@ -35,7 +36,7 @@ def get_measurement_unit_options() -> list[dict[str, str]]:
         }
         for measurement_unit in state.measurement_units.classifications
     ]
-    dropdown_options.insert(0, {"title": DROPDOWN_DESELECT_OPTION, "id": ""})
+    dropdown_options.insert(0, {"title": DROPDOWN_DESELECT_OPTION, "id": DELETE_SELECTED})
     return dropdown_options
 
 
@@ -48,7 +49,7 @@ def get_unit_type_options() -> list[dict[str, str]]:
         }
         for unit_type in state.unit_types.classifications
     ]
-    dropdown_options.insert(0, {"title": DROPDOWN_DESELECT_OPTION, "id": ""})
+    dropdown_options.insert(0, {"title": DROPDOWN_DESELECT_OPTION, "id": DELETE_SELECTED})
     return dropdown_options
 
 
