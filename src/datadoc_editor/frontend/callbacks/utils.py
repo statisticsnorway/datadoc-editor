c88dbf812e1fab34879609140ad1fae26330e6b0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-09 20:29:30 +0200

    linting

--- src/datadoc_editor/frontend/callbacks/utils.py
+++ src/datadoc_editor/frontend/callbacks/utils.py
@@ -378,7 +378,10 @@ def render_multidropdown_row(
                         description=field.date_description,
                     ),
                     html.Button(
-                        "", id=button_id, className="multidropdown-delete-button", **{"aria-label": "Delete row"}
+                        "",
+                        id=button_id,
+                        className="multidropdown-delete-button",
+                        **{"aria-label": "Delete row"},
                     ),
                 ],
                 className="date-button-row",
