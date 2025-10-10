76c9b63a554f21599927f04341dd6837e1662ab5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-10 08:52:31 +0200

    pre commit

--- tests/frontend/fields/test_display_variables.py
+++ tests/frontend/fields/test_display_variables.py
@@ -1,9 +1,16 @@
 import pytest
 
 from datadoc_editor import state
-from datadoc_editor.constants import DELETE_SELECTED, NUM_GLOBAL_EDITABLE_VARIABLES
+from datadoc_editor.constants import DELETE_SELECTED
+from datadoc_editor.constants import NUM_GLOBAL_EDITABLE_VARIABLES
 from datadoc_editor.frontend.fields.display_base import DROPDOWN_DESELECT_OPTION
-from datadoc_editor.frontend.fields.display_variables import DISPLAY_VARIABLES, GLOBAL_EDITABLE_VARIABLES_METADATA_AND_DISPLAY_NAME, GLOBAL_VARIABLES, VariableIdentifiers, get_unit_type_options
+from datadoc_editor.frontend.fields.display_variables import DISPLAY_VARIABLES
+from datadoc_editor.frontend.fields.display_variables import (
+    GLOBAL_EDITABLE_VARIABLES_METADATA_AND_DISPLAY_NAME,
+)
+from datadoc_editor.frontend.fields.display_variables import GLOBAL_VARIABLES
+from datadoc_editor.frontend.fields.display_variables import VariableIdentifiers
+from datadoc_editor.frontend.fields.display_variables import get_unit_type_options
 from tests.conftest import CODE_LIST_DIR
 from tests.utils import TEST_RESOURCES_DIRECTORY
 
@@ -30,9 +37,13 @@ def test_get_unit_type_options(
     state.unit_types.wait_for_external_result()
     assert get_unit_type_options() == expected
 
+
 def test_global():
     assert len(GLOBAL_VARIABLES) == NUM_GLOBAL_EDITABLE_VARIABLES
 
 
 def test_global_tuple():
-    assert (DISPLAY_VARIABLES[VariableIdentifiers.UNIT_TYPE].identifier,DISPLAY_VARIABLES[VariableIdentifiers.UNIT_TYPE].display_name ) in GLOBAL_EDITABLE_VARIABLES_METADATA_AND_DISPLAY_NAME
+    assert (
+        DISPLAY_VARIABLES[VariableIdentifiers.UNIT_TYPE].identifier,
+        DISPLAY_VARIABLES[VariableIdentifiers.UNIT_TYPE].display_name,
+    ) in GLOBAL_EDITABLE_VARIABLES_METADATA_AND_DISPLAY_NAME
