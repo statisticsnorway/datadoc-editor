27d958c7fa8325870a68232cd57045abab95c62e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-10 14:10:44 +0200

    update test

--- tests/frontend/fields/test_display_variables.py
+++ tests/frontend/fields/test_display_variables.py
@@ -1,10 +1,10 @@
 import pytest
 
 from datadoc_editor import state
-from datadoc_editor.constants import DELETE_SELECTED
+from datadoc_editor.constants import DELETE_SELECTED, DROPDOWN_DELETE_OPTION
 from datadoc_editor.constants import NUM_GLOBAL_EDITABLE_VARIABLES
 from datadoc_editor.frontend.fields.display_base import DROPDOWN_DESELECT_OPTION
-from datadoc_editor.frontend.fields.display_variables import DISPLAY_VARIABLES
+from datadoc_editor.frontend.fields.display_variables import DISPLAY_VARIABLES, get_unit_type_options_with_delete
 from datadoc_editor.frontend.fields.display_variables import (
     GLOBAL_EDITABLE_VARIABLES_METADATA_AND_DISPLAY_NAME,
 )
@@ -21,7 +21,7 @@ from tests.utils import TEST_RESOURCES_DIRECTORY
         (
             TEST_RESOURCES_DIRECTORY / CODE_LIST_DIR / "code_list_nb.csv",
             [
-                {"title": DROPDOWN_DESELECT_OPTION, "id": DELETE_SELECTED},
+                {"title": DROPDOWN_DESELECT_OPTION, "id": ""},
                 {"title": "Adresse", "id": "01"},
                 {"title": "Arbeidsulykke", "id": "02"},
                 {"title": "Bolig", "id": "03"},
@@ -36,6 +36,28 @@ def test_get_unit_type_options(
     state.unit_types = code_list_fake_structure
     state.unit_types.wait_for_external_result()
     assert get_unit_type_options() == expected
+
+@pytest.mark.parametrize(
+    ("code_list_csv_filepath_nb", "expected"),
+    [
+        (
+            TEST_RESOURCES_DIRECTORY / CODE_LIST_DIR / "code_list_nb.csv",
+            [
+                {"title": DROPDOWN_DELETE_OPTION, "id": DELETE_SELECTED},
+                {"title": "Adresse", "id": "01"},
+                {"title": "Arbeidsulykke", "id": "02"},
+                {"title": "Bolig", "id": "03"},
+            ],
+        ),
+    ],
+)
+def test_get_unit_type_options_with_delete(
+    code_list_fake_structure,
+    expected,
+):
+    state.unit_types = code_list_fake_structure
+    state.unit_types.wait_for_external_result()
+    assert get_unit_type_options_with_delete() == expected
 
 
 def test_global():
